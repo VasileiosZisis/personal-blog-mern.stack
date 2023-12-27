e62f3687318cb4c048d77e2b80aacaad90ca11f7
GoneAway <vasil.zisis@gmail.com>
2023-12-27 21:20:37 +0200

    first set-up

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Server is ready'));
+
+app.listen(port, () => console.log(`listening ${port}`));
