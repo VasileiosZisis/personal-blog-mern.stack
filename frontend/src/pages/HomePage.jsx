14e8f6eba5cf0a196be8ebb756875ec6a88b4a63
GoneAway <vasil.zisis@gmail.com>
2024-01-19 22:31:36 +0200

    re-styling homepage, added carousel

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import './HomePage.css'
+import 'swiper/css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css/navigation'
+import { Autoplay, Navigation } from 'swiper/modules'
 import Gallery from '../assets/gallery.webp'
 import Game from '../assets/ar6dy.png'
 import Book from '../assets/true.jpg'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const HomePage = () => {
   return (
@@ -12,6 +17,82 @@ const HomePage = () => {
       </section>
       <section className='section-currently'>
         <h2 className='currently-h2'>Currently</h2>
+        <div className='swiper-container'>
+          <Swiper
+            className='mySwiper'
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false
+            }}
+            spaceBetween={20}
+            loop={true}
+            slidesPerView={3}
+            navigation={true}
+            modules={[Autoplay, Navigation]}
+          >
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Playing</p>
+                <p className='card-subtitle'>Doom Eternal</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Game} />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Playing</p>
+                <p className='card-subtitle'>Doom Eternal</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Game} />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Reading</p>
+                <p className='card-subtitle'>True Believer</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Book} />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Reading</p>
+                <p className='card-subtitle'>True Believer</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Book} />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Reading</p>
+                <p className='card-subtitle'>True Believer</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Book} />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className='currently-card'>
+                <p className='card-title'>Playing</p>
+                <p className='card-subtitle'>Doom Eternal</p>
+                <hr className='card-hr' />
+                <div>
+                  <img className='card-img' src={Game} />
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        {/* 
         <div className='currently-card-container-parent'>
           <div className='currently-card-container-child'>
             <div className='currently-card-container'>
@@ -19,7 +100,7 @@ const HomePage = () => {
                 <p className='card-title'>Playing</p>
                 <p className='card-subtitle'>Doom Eternal</p>
                 <hr className='card-hr' />
-                <div className='card-img-container'>
+                <div>
                   <img className='card-img' src={Game} />
                 </div>
               </div>
@@ -27,7 +108,7 @@ const HomePage = () => {
                 <p className='card-title'>Reading</p>
                 <p className='card-subtitle'>True Believer</p>
                 <hr className='card-hr' />
-                <div className='card-img-container'>
+                <div>
                   <img className='card-img' src={Book} />
                 </div>
               </div>
@@ -35,7 +116,7 @@ const HomePage = () => {
                 <p className='card-title'>Playing</p>
                 <p className='card-subtitle'>Doom Eternal</p>
                 <hr className='card-hr' />
-                <div className='card-img-container'>
+                <div>
                   <img className='card-img' src={Game} />
                 </div>
               </div>
@@ -43,16 +124,16 @@ const HomePage = () => {
                 <p className='card-title'>Reading</p>
                 <p className='card-subtitle'>True Believer</p>
                 <hr className='card-hr' />
-                <div className='card-img-container'>
+                <div>
                   <img className='card-img' src={Book} />
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
       <section className='section-latest'>
-        <h2 className='latest-h2'>Latest</h2>
+        {/* <h2 className='latest-h2'>Latest</h2>
         <div className='article-section'>
           <Link className='article-container'>
             <div className='article-img-container'>
@@ -98,7 +179,7 @@ const HomePage = () => {
               <hr className='article-link-hr' />
             </div>
           </Link>
-        </div>
+        </div> */}
       </section>
     </main>
   )
