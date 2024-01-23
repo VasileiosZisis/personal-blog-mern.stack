f6fef3bd3c6243da493e91c4bb49e595c243c272
GoneAway <vasil.zisis@gmail.com>
2024-01-23 22:22:11 +0200

    homepage styles

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -92,94 +92,44 @@ const HomePage = () => {
             </SwiperSlide>
           </Swiper>
         </div>
-        {/* 
-        <div className='currently-card-container-parent'>
-          <div className='currently-card-container-child'>
-            <div className='currently-card-container'>
-              <div className='currently-card'>
-                <p className='card-title'>Playing</p>
-                <p className='card-subtitle'>Doom Eternal</p>
-                <hr className='card-hr' />
-                <div>
-                  <img className='card-img' src={Game} />
-                </div>
-              </div>
-              <div className='currently-card'>
-                <p className='card-title'>Reading</p>
-                <p className='card-subtitle'>True Believer</p>
-                <hr className='card-hr' />
-                <div>
-                  <img className='card-img' src={Book} />
-                </div>
-              </div>
-              <div className='currently-card'>
-                <p className='card-title'>Playing</p>
-                <p className='card-subtitle'>Doom Eternal</p>
-                <hr className='card-hr' />
-                <div>
-                  <img className='card-img' src={Game} />
-                </div>
-              </div>
-              <div className='currently-card'>
-                <p className='card-title'>Reading</p>
-                <p className='card-subtitle'>True Believer</p>
-                <hr className='card-hr' />
-                <div>
-                  <img className='card-img' src={Book} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </section>
       <section className='section-latest'>
-        {/* <h2 className='latest-h2'>Latest</h2>
-        <div className='article-section'>
-          <Link className='article-container'>
-            <div className='article-img-container'>
-              <img className='article-img' src={Game} />
+        <h2 className='latest-h2'>Latest</h2>
+        <div className='article-card-container'>
+          <div className='article-card'>
+            <img className='article-card-img' src={Game} />
+            <hr className='article-hr' />
+            <p className='article-title'>Death Stranding Surprised Me</p>
+            <p className='article-subtitle'>and I know it shouldn't</p>
+            <div className='article-link-container'>
+              <Link href='/' className='article-link'>
+                <button>READ MORE</button>
+              </Link>
             </div>
-            <div className='article-text-container'>
-              <h1>Death Stranding Surprised Me</h1>
-              <time dateTime='2001-05-15T19:00'>May 15</time>
-              <p className='article-p'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna
-              </p>
-              <hr className='article-link-hr' />
-              <p className='article-link'>READ MORE</p>
-              <hr className='article-link-hr' />
+          </div>
+          <div className='article-card'>
+            <img className='article-card-img' src={Book} />
+            <hr className='article-hr' />
+            <p className='article-title'>Death Stranding Surprised Me</p>
+            <p className='article-subtitle'>and I know it shouldn't</p>
+            <div className='article-link-container'>
+              <Link href='/' className='article-link'>
+                <button>READ MORE</button>
+              </Link>
             </div>
-          </Link>
-          <Link className='article-container'>
-            <div className='article-img-container'>
-              <img className='article-img' src={Book} />
+          </div>
+          <div className='article-card'>
+            <img className='article-card-img' src={Game} />
+            <hr className='article-hr' />
+            <p className='article-title'>Death Stranding Surprised Me</p>
+            <p className='article-subtitle'>and I know it shouldn't</p>
+            <div className='article-link-container'>
+              <Link href='/' className='article-link'>
+                <button>READ MORE</button>
+              </Link>
             </div>
-            <div className='article-text-container'>
-              <h1>Death Stranding Surprised Me</h1>
-              <time dateTime='2001-05-15T19:00'>May 15</time>
-              <p className='article-p'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna Lorem ipsum
-                dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna
-              </p>
-              <hr className='article-link-hr' />
-              <p className='article-link'>READ MORE</p>
-              <hr className='article-link-hr' />
-            </div>
-          </Link>
-        </div> */}
+          </div>
+        </div>
       </section>
     </main>
   )
